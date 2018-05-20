809e8b969ee4e0b31b8adfe516048577f210be9a
zumerok <37517704+zumerok@users.noreply.github.com>
2018-05-20 21:51:50 +0300

    Add files via upload

--- nailparis/js/main.js
+++ nailparis/js/main.js
@@ -1,20 +1,13 @@
+/* scrolling */
 $(function(){
   $('a[href^="#"]').on('click', function(event) {
-    // отменяем стандартное действие
-    event.preventDefault();
-    
+    event.preventDefault();   
     var sc = $(this).attr("href"),
         dn = $(sc).offset().top;
-    /*
-    * sc - в переменную заносим информацию о том, к какому блоку надо перейти
-    * dn - определяем положение блока на странице
-    */
-    
     $('html, body').animate({scrollTop: dn}, 500);
-    
-    /*
-    * 1000 скорость перехода в миллисекундах
-    */
   });
 });
- 
+/* slider */
+var elem = document.getElementById('mySwipe');
+window.mySwipe = Swipe(elem, {
+});
